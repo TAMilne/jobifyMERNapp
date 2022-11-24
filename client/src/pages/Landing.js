521ea9c8d528afe7cb0ecd0b3c2890f39ffe70f4
Travis Milne <Travismilne8@gmail.com>
2022-11-24 12:11:19 -0700

    Landing Page Completed

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,24 @@
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/Testing'
+
+const Landing = () => {
+  return <Wrapper>
+            <nav>
+                <img src={logo} alt='jobify' className='logo'/>
+            </nav>
+            <div className='container page'>
+                <div className='info'>
+                    <h1>Job <span>Tracking</span> app
+                    </h1>
+                    <p>
+                    I'm baby locavore jean shorts cray photo booth squid ethical. Hella narwhal affogato, mustache succulents woke messenger bag viral jianbing stumptown gastropub. 8-bit brunch mlkshk, meggings truffaut fam affogato. Normcore flexitarian occupy 3 wolf moon kogi lo-fi craft beer mumblecore pickled iceland portland tote bag vinyl. Echo park meh cold-pressed, VHS fingerstache hot chicken tofu health goth post-ironic.
+                    </p>
+                    <button className='btn btn-hero'>Login/Register</button>
+                </div>
+                <img src={main} alt="job hunt" className="img main-img" />
+            </div>
+        </Wrapper>
+}
+
+export default Landing

63fab104a83ccf1849bfeb68346ec30abe103bd7
Travis Milne <Travismilne8@gmail.com>
2022-12-19 16:01:00 -0700

    Auth & Job routes done

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
 import express from 'express'
-
 const app = express()
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+//Db Connection and authenticate USer
+import connectDB from './db/connect.js'
+
+//routers
+import authRouter from './routes/authRoute.js'
+import jobsRouter from './routes/jobsRoute.js'
 
 // Middleware
 import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddelware from './middleware/error-handler.js'
 
-app.get('/', (req, res)=>{
+app.use(express.json())
+
+app.get('/', (req, res) => {
     res.send("Welcome!")
 })
 
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
+
 app.use(notFoundMiddleware)
+app.use(errorHandlerMiddelware)
 
 const port = process.env.PORT || 5000
 
-app.listen(port, ()=>{
-    console.log(`Server is listenig on port ${port}...`)
-})
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => {
+            console.log(`Server is listenig on port ${port}...`)
+        })
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+start()
